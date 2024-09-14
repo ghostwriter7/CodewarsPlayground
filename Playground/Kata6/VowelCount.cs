e395c3fe4a98d2da33981dd5bd86ecce4542453f
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 13:38:53 +0200

    VowelCount

---
+++ Playground/Kata6/VowelCount.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+public static class VowelCount
+{
+    private static char[] vowels = ['a', 'e', 'i', 'o', 'u'];
+    public static int GetVowelCount(string str) => str.Count(vowels.Contains);
+}
+
+[TestFixture]
+public class VowelCountTest
+{
+    [TestCase("hello", ExpectedResult = 2)]
+    [TestCase("world", ExpectedResult = 1)]
+    [TestCase("this is some funky sentence", ExpectedResult = 8)]
+    public int GetVowelCount_ForSentence_ShouldReturnCountOfVowels(string sentence) =>
+        VowelCount.GetVowelCount(sentence);
+
+}
