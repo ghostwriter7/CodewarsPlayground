6e5195c66940b2e05dd848c7a1bfd3b74f9b74b3
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 13:33:11 +0200

    DigitalRoot

---
+++ Playground/Kata6/DigitalRoot.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+public static class DigitalRoot
+{
+    public static int Get(long n)
+    {
+        var value = n.ToString()
+            .Sum(character => int.Parse(character.ToString()));
+
+        if (value.ToString().Length > 1)
+            return Get(value);
+        return value;
+    }
+}
+
+[TestFixture]
+public class DigitalRootTests
+{
+    [TestCase(123456, ExpectedResult = 3)]
+    [TestCase(999, ExpectedResult = 9)]
+    [TestCase(10001, ExpectedResult = 2)]
+    public int Get_ForLongNumber_ShouldReturnItsDigitalRoot(long value)
+    {
+        return DigitalRoot.Get(value);
+    }
+}
