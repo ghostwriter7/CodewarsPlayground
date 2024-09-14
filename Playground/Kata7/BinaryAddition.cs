5988c4d9156486f95f52d296a1e82f1b1237c5b8
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 13:43:15 +0200

    BinaryAddition

---
+++ Playground/Kata7/BinaryAddition.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+public static class BinaryAddition
+{
+    public static string AddBinary(int a, int b) => (a + b).ToString("B");
+}
+
+[TestFixture]
+public class BinaryAdditionTests
+{
+    [TestCase(1, 1, ExpectedResult = "10")]
+    [TestCase(5, 9, ExpectedResult = "1110")]
+    public string AddBinary_ForTwoIntegerInputs_ShouldReturnItsSumAsBinary(int a, int b) =>
+        BinaryAddition.AddBinary(a, b);
+}
