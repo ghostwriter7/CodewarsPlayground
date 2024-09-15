9cb2059b22aba8c0cd4ee591d54bfc478278b876
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-15 13:13:57 +0200

    CreatePhoneNumber

---
+++ Playground/Kata6/CreatePhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+/// <summary>
+/// Write a function that accepts an array of 10 integers (between 0 and 9),
+/// that returns a string of those numbers in the form of a phone number.
+/// "(123) 456-7890"
+/// </summary>
+public static class CreatePhoneNumber
+{
+    public static string Create(int[] digits) => long.Parse(string.Concat(digits)).ToString("(000) 000-0000");
+}
+
+[TestFixture]
+public class CreatePhoneNumberTests
+{
+    [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, ExpectedResult = "(111) 111-1111")]
+    [TestCase(new int[] { 0, 0, 7, 6, 6, 6, 1, 2, 3, 4 }, ExpectedResult = "(007) 666-1234")]
+    public string Create_For10Integers_ShouldReturnPhoneNumber(int[] digits) => CreatePhoneNumber.Create(digits);
+}
