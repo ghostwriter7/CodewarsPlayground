3834c542ce0e6b7fc33a4b235c6c4ac8ec8753f9
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-15 11:49:59 +0200

    FindNextPerfectSquareRoot

---
+++ Playground/Kata7/NextPerfectSquare.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+/// <summary>
+/// Finds the next integral perfect square after the one passed as a parameter.
+/// Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+/// </summary>
+public static class NextPerfectSquare
+{
+    public static long Get(long input)
+    {
+        var sqrt = Math.Sqrt(input);
+        if (sqrt * sqrt != input)
+            return -1;
+
+        var nextInt = (long) sqrt  + 1;
+        
+        return nextInt * nextInt;
+    }
+}
+
+[TestFixture]
+public class NextPerfectSquareTests
+{
+    [TestCase(10, ExpectedResult = -1)]
+    [TestCase(144, ExpectedResult = 169)]
+    [TestCase(81, ExpectedResult = 100)]
+    [TestCase(4503599627370497L, ExpectedResult = -1)]
+    public long Get_ForIntInput_ShouldReturnNextIntPerfectSquare(long input) => NextPerfectSquare.Get(input);
+}
