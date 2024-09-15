3651276cd67c08a87eaf96b9a64f2ac3ed1a45fc
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-15 14:05:46 +0200

    BitCounting implemented

---
+++ Playground/Kata6/BitCounting.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Numerics;
+
+namespace DefaultNamespace;
+
+/// <summary>
+/// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+/// You can guarantee that input is non-negative.
+/// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+/// </summary>
+public static class BitCounting
+{
+    public static int Count(int input)
+        => Convert.ToString(input, 2).Count((character) => character == '1');
+}
+
+[TestFixture]
+public class BitCountingTests
+{
+    [TestCase(1234, ExpectedResult = 5)]
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(12525589, ExpectedResult = 11)]
+    [TestCase(int.MaxValue, ExpectedResult = 31)]
+    public int Count_ForInteger_ShouldReturnCountOfOnesInItsBinaryRepresentation(int input) => BitCounting.Count(input);
+}
