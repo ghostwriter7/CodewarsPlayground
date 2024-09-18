96a42421c1c50197913585f86ae71d58a8928bca
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-18 21:52:18 +0200

    BreakCamelCase kata implemented

---
+++ Playground/Kata6/BreakCamelCase.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace DefaultNamespace;
+
+public static class BreakCamelCase
+{
+    public static string Convert(string input) => input.Aggregate(new StringBuilder(), (sb, character) =>
+    {
+        if (char.IsUpper(character))
+            sb.Append(' ');
+        sb.Append(character);
+        return sb;
+    }).ToString();
+}
+
+[TestFixture]
+public class BreakCamelCaseTests
+{
+    [TestCase("camelCasing", ExpectedResult = "camel Casing")]
+    [TestCase("identifier", ExpectedResult = "identifier")]
+    [TestCase("", ExpectedResult = "")]
+    public string Convert_ForStringInput_ShouldReturnStringWithSpaces(string input) => BreakCamelCase.Convert(input);
+}
