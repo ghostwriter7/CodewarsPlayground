b2f53e7715f3feaa0ad0e4fd7c97a57819b58101
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-21 10:11:43 +0200

    Setup for a new Kata done

---
+++ Playground/Kata4/CatchingInterestingCarNumbers.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Playground.Kata4;
+
+public static class CatchingInterestingCarNumbers
+{
+    public static int IsInteresting(int number, List<int> awesomePhrases)
+    {
+        return -1;
+    }
+}
+
+[TestFixture]
+public class CatchingInterestingCarNumbersTests
+{
+    [Test]
+    public void ShouldWorkTest()
+    {
+        Assert.That(0, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(3, new List<int>() { 1337, 256 })));
+        Assert.That(1, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(1336, new List<int>() { 1337, 256 })));
+        Assert.That(2, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(1337, new List<int>() { 1337, 256 })));
+        Assert.That(0, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(11208, new List<int>() { 1337, 256 })));
+        Assert.That(1, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(11209, new List<int>() { 1337, 256 })));
+        Assert.That(2, Is.EqualTo(CatchingInterestingCarNumbers.IsInteresting(11211, new List<int>() { 1337, 256 })));
+    }
+}
